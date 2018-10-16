938cd683bdeddbdce10c5e7674307bf1eb31fb8a
Bart Enkelaar <benkelaar@bol.com>
2018-10-16 11:48:16 +0200

    Add psql load step

--- dags/dag.py
+++ dags/dag.py
@@ -2,7 +2,7 @@ import datetime as dt
 
 from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
-
+from godatadriven.operators.postgres_to_gcs import PostgresToGoogleCloudStorageOperator
 
 dag = DAG(
     dag_id="my_first_dag",
@@ -24,3 +24,14 @@ def print_exec_date(**context):
 my_task = PythonOperator(
     task_id="task_name", python_callable=print_exec_date, provide_context=True, dag=dag
 )
+
+psql_to_gcs = PostgresToGoogleCloudStorageOperator(
+    task_id="read_postgres",
+    postgres_conn_id="postgres_training",
+    sql="select * from gdd.land_registry_price_paid_uk where transfer_date = '{{ ds }}'::date",
+    bucket="gs://airflow-training-simple-dag/",
+    filename="training-price-paid-uk/{{ ds }}",
+    dag=dag
+)
+
+psql_to_gcs >> my_task
