66eb40802fa3f17214fd66148fd5a5f3511cd289
Bart Enkelaar <benkelaar@bol.com>
2018-10-16 13:33:09 +0200

    Add Cluster operation

--- dags/dag.py
+++ dags/dag.py
@@ -1,9 +1,15 @@
 import datetime as dt
 
 from airflow import DAG
+from airflow.utils.trigger_rule import TriggerRule
+from airflow.contrib.operators.dataproc_operator import (
+    DataprocClusterCreateOperator,
+    DataprocClusterDeleteOperator,
+    DataProcPySparkOperator
+)
 from godatadriven.operators.postgres_to_gcs import PostgresToGoogleCloudStorageOperator
 
-dag = DAG(
+with DAG(
     dag_id="training-money-maker",
     schedule_interval="30 7 * * *",
     default_args={
@@ -13,18 +19,38 @@ dag = DAG(
         "email_on_failure": True,
         "email": "airflow_errors@myorganisation.com",
     },
-)
+) as dag:
+    psql_to_gcs = PostgresToGoogleCloudStorageOperator(
+        task_id="read_postgres",
+        postgres_conn_id="postgres_training",
+        sql="select * from land_registry_price_paid_uk where transfer_date = '{{ ds }}'::date",
+        bucket="airflow-training-simple-dag",
+        filename="training-price-paid-uk/{{ ds }}/land_registry.json"
+    )
 
+    cluster_name = "cluster-{{ ds }}"
+    gcs_project_id = "airflowbolcom-544f36a42f5c0d9d"
 
-def print_exec_date(**context):
-    print(context["execution_date"])
+    create_cluster = DataprocClusterCreateOperator(
+        task_id="create_cluster",
+        cluster_name=cluster_name,
+        project_id=gcs_project_id,
+        num_workers=2,
+        zone="EU"
+    )
 
+    cloud_analytics = DataProcPySparkOperator(
+        task_id="analyze-data",
+        main="gs://gdd-training/build_statistics.py",
+        cluster_name=cluster_name,
+        arguments={}
+    )
 
-psql_to_gcs = PostgresToGoogleCloudStorageOperator(
-    task_id="read_postgres",
-    postgres_conn_id="postgres_training",
-    sql="select * from land_registry_price_paid_uk where transfer_date = '{{ ds }}'::date",
-    bucket="airflow-training-simple-dag",
-    filename="training-price-paid-uk/{{ ds }}/land_registry.json",
-    dag=dag
-)
+    delete_cluster = DataprocClusterDeleteOperator(
+        task_id="delete_cluster",
+        cluster_name=cluster_name,
+        project_id=gcs_project_id,
+        trigger_rule=TriggerRule.ALL_DONE
+    )
+
+    psql_to_gcs >> create_cluster >> cloud_analytics >> delete_cluster
