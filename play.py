fe523279de254749b311890a5933afd454720225
Bart Enkelaar <benkelaar@bol.com>
2018-10-17 10:48:44 +0200

    Remove play.py

--- play.py
+++
@@ -1,5 +0,0 @@
-from airflow.hooks.http_hook import HttpHook
-
-result = HttpHook(http_conn_id='test', method='GET').run(endpoint="https://europe-west1-gdd-airflow-training.cloudfunctions.net/airflow-training-transform-valutas?date=2018-10-10&from=GBP&to=USD")
-
-print(str(result))
